b0928419438d3abb2533a393405653b571a1f5fe
Carlan Calazans <carlancalazans@gmail.com>
2019-02-25 17:02:54 +0000

    Ticket 12: User Report
    - Updated dao

--- mflix/db.py
+++ mflix/db.py
@@ -543,9 +543,16 @@ def most_active_commenters():
     """
     # TODO: User Report
     # Return the 20 users who have commented the most on MFlix.
-    pipeline = []
+    pipeline = [
+        {
+            "$sortByCount": "$email"
+        },
+        {
+            "$limit": 20
+        }
+    ]
 
-    rc = db.comments.read_concern # you may want to change this read concern!
+    rc = ReadConcern(level="majority") # you may want to change this read concern!
     comments = db.comments.with_options(read_concern=rc)
     result = comments.aggregate(pipeline)
     return list(result)
