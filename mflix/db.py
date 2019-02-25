0c2892129acdc3209d5cdc1d781d7556d1b245e4
Carlan Calazans <carlancalazans@gmail.com>
2019-02-25 13:11:58 +0000

    Ticket 4: Paging
    - Updated dao

--- mflix/db.py
+++ mflix/db.py
@@ -213,7 +213,7 @@ def get_movies(filters, page, movies_per_page):
 
     # TODO: Paging
     # Use the cursor to only return the movies that belong on the current page.
-    movies = cursor.limit(movies_per_page)
+    movies = cursor.skip(page * movies_per_page).limit(movies_per_page)
 
     return (list(movies), total_num_movies)
 
